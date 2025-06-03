3a7677df9bd04bb41e59ffa41c5f6fdb2622264b
Jeffrey Bekker <120750708+Jeffreybekker@users.noreply.github.com>
2025-06-03 15:33:50 +0200

    Can retrieve data from supabase

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import psycopg2
 import os
 from dotenv import load_dotenv
-import psycopg2
+
+load_dotenv()
+
+conn = psycopg2.connect(os.getenv("SUPABASE_CONNECTION"))
+
+cur = conn.cursor()
+cur.execute("SELECT voornaam, huwelijksdatum, geboortedatum FROM familiegegevens;") # Haalt de data op van Supabase. 
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+conn.close()
