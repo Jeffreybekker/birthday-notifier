f91342d0be54989d7ffd87fe56d5867adac4748c
Jeffrey Bekker <120750708+Jeffreybekker@users.noreply.github.com>
2025-06-03 15:43:57 +0200

    Flake8 added to check if the code is PEP8

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 conn = psycopg2.connect(os.getenv("SUPABASE_CONNECTION"))
 
 cur = conn.cursor()
-cur.execute("SELECT voornaam, huwelijksdatum, geboortedatum FROM familiegegevens;") # Haalt de data op van Supabase. 
+# Haalt data op van Supabase met een SQL command.
+cur.execute(
+    "SELECT voornaam, huwelijksdatum, geboortedatum FROM familiegegevens;"
+    )
 rows = cur.fetchall()
 
 for row in rows:
